0d9fd9f622ab37f4f2cf98af43ba040f44512fb4
inmarelibero <inmarelibero@gmail.com>
2013-02-06 12:32:50 +0100

    added compatibility with Symfony v2.0.*

--- Form/MetatagType.php
+++ Form/MetatagType.php
@@ -3,7 +3,7 @@
 namespace Copiaincolla\MetaTagsBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormBuilder;
 use Copiaincolla\MetaTagsBundle\Loader\UrlsLoader;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -18,7 +18,7 @@ class MetatagType extends AbstractType
         $this->urlsLoader = $urlsLoader;
     }
 
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilder $builder, array $options)
     {
         $config = $this->urlsLoader->getConfig();
 
